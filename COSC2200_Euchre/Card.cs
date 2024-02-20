92accd453cd392b42192f09c5b45b0eb338e533f
Nathan <nathaniel.wesener@dcmail.ca>
2024-02-20 16:11:26 -0500

    Added some thigs to the card class and created a new utilities method.

--- COSC2200_Euchre/Card.cs
+++ COSC2200_Euchre/Card.cs
@@ -8,27 +8,34 @@ namespace COSC2200_Euchre
 {
     internal class Card
     {
-        int cardSuiteNum;
-        int cardRankNum;
-        string cardSuiteStr;
-        string cardRankStr;
-        bool isRedd;
-        
+        public int cardSuiteNum;
+        public int cardRankNum;
+        public  string cardSuiteStr;
+        public string cardRankStr;
+        public bool isRed;
+
         // TODO: finish comment - NW
         /// <summary>
         /// Constructer for a card object. - NW
         /// </summary>
-        /// <param name="cardSuitNum"></param>
+        /// <param name="cardSuiteNum"></param>
         /// <param name="cardRankNum"></param>
         /// <param name="cardSuiteStr"></param>
         /// <param name="cardRankStr"></param>
-        public Card(int cardSuitNum, int cardRankNum, string cardSuiteStr, string cardRankStr bool isRed) 
+        public Card(int cardSuiteNum, int cardRankNum, string cardSuiteStr, string cardRankStr, bool isRed)
         {
-            this.cardSuiteNum = cardSuitNum;
+            this.cardSuiteNum = cardSuiteNum;
             this.cardRankNum = cardRankNum;
             this.cardSuiteStr = cardSuiteStr;
             this.cardRankStr = cardRankStr;
-            this.isRedd = isRed;
+            this.isRed = isRed;
+        }
+
+        public string toString()
+        {
+            string cardString;
+            cardString = this.cardRankStr + " of " + this.cardSuiteStr;
+            return cardString;
         }
     }
 }
