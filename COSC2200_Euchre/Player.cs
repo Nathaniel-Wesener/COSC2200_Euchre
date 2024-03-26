7555a4d757d6ff1a6c3fdddfb710948f44ac3149
Nathan <nathaniel.wesener@dcmail.ca>
2024-03-26 17:00:52 -0400

    Added an add points function to the player class.

--- COSC2200_Euchre/Player.cs
+++ COSC2200_Euchre/Player.cs
@@ -24,5 +24,25 @@ namespace COSC2200_Euchre
             this.isAI = isAI;
         }
 
+        public void addPoints()
+        {
+            if (isMaker && tricksWon < 5)
+            {
+                points++;
+            }
+            else if (isMaker)
+            {
+                points += 2;
+            }
+            else if (tricksWon < 5)
+            {
+                points += 2;
+            }
+            else
+            {
+                points += 4;
+            }
+        }
+
     }
 }
